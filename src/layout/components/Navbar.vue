fb8ff4aff9b63bcf2f3aa5fa646272d85a093dca
1196683884@qq.com <1196683884@qq.com>
2025-06-19 18:35:16 +0800

    fix 完善部分navbar页面

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,14 +1,32 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="navbar"></div>
+  <div class="navbar">
+    <div class="leftTit">测试</div>
+    <div class="rightTit">
+      <img
+        src="https://picsum.photos/50/50"
+        alt="用户头像"
+        class="w-8 h-8 rounded-full object-cover"
+        style="width: 50px; height: 50px"
+      />
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .navbar {
   height: 50px;
   width: 100%;
   box-shadow: 0 2px 4px #f3f4f5;
   background-color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  .rightTit {
+    // border-radius: 25px;
+    // overflow: hidden;
+  }
 }
 </style>
