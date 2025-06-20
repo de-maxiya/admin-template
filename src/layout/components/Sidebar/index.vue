c59847ec22b5771a87ffe13613bcc69758db4aef
1196683884@qq.com <1196683884@qq.com>
2025-06-20 19:01:26 +0800

    fix

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -68,6 +68,59 @@ onMounted(() => {
         </el-sub-menu>
       </template>
     </el-menu>
+    <el-menu
+      active-text-color="#ffd04b"
+      background-color="#212c3d"
+      class="el-menu-vertical-demo"
+      :default-active="'/'"
+      text-color="#fff"
+      style="border: none"
+      :collapse="isCollapse"
+      router
+      @open="handleOpen"
+      @close="handleClose"
+    >
+      <!-- 第一层：系统概览 -->
+      <el-menu-item index="/">
+        <template #title>
+          <el-icon style="color: white"><House /></el-icon>
+          <span>系统概览</span>
+        </template>
+      </el-menu-item>
+
+      <!-- 第二层：数据管理模块 -->
+      <el-sub-menu index="data">
+        <template #title>
+          <i class="el-icon-s-data"></i>
+          <span>数据管理</span>
+        </template>
+        <el-menu-item index="/data/list">数据列表</el-menu-item>
+        <el-menu-item index="/data/import">数据导入</el-menu-item>
+        <el-menu-item index="/data/export">数据导出</el-menu-item>
+      </el-sub-menu>
+
+      <!-- 第二层：用户管理模块 -->
+      <el-sub-menu index="user">
+        <template #title>
+          <i class="el-icon-s-custom"></i>
+          <span>用户管理</span>
+        </template>
+        <el-menu-item index="/user/list">用户列表</el-menu-item>
+        <el-menu-item index="/user/role">角色管理</el-menu-item>
+        <el-menu-item index="/user/permission">权限配置</el-menu-item>
+      </el-sub-menu>
+
+      <!-- 第二层：系统设置模块 -->
+      <el-sub-menu index="system">
+        <template #title>
+          <i class="el-icon-s-tools"></i>
+          <span>系统设置</span>
+        </template>
+        <el-menu-item index="/system/settings">基本设置</el-menu-item>
+        <el-menu-item index="/system/logs">操作日志</el-menu-item>
+        <el-menu-item index="/system/backup">数据备份</el-menu-item>
+      </el-sub-menu>
+    </el-menu>
   </div>
 </template>
 
