a97d0f72a3738a15d81f256ec8883403eb8b5824
1196683884@qq.com <1196683884@qq.com>
2025-06-20 19:06:27 +0800

    fix proxy

--- vite.config.ts
+++ vite.config.ts
@@ -20,13 +20,13 @@ export default defineConfig({
   },
   server: {
     open: true, // 自动打开浏览器
-    proxy: {
-      // 匹配 /api 开头请求
-      // '/api': {
-      //   target: 'http://api.example.com',
-      //   changeOrigin: true,
-      //   rewrite: (path) => path.replace(/^\/api/, ''), // 重写路径，类似 Vue CLI 的 pathRewrite
-      // },
-    },
+    // proxy: {
+    // 匹配 /api 开头请求
+    // '/api': {
+    //   target: 'http://api.example.com',
+    //   changeOrigin: true,
+    //   rewrite: (path) => path.replace(/^\/api/, ''), // 重写路径，类似 Vue CLI 的 pathRewrite
+    // },
+    // },
   },
 })
