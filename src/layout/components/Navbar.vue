9e0df7396eab2e86647fdc7c1e2c2235b9cd07c6
1196683884@qq.com <1196683884@qq.com>
2025-06-25 15:18:05 +0800

    fix 新增部分动态路由

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,14 +1,65 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { RouteLocationNormalized } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
+import { watch } from 'vue'
+
+class RouteStack {
+  private stack: RouteLocationNormalized[] = []
+  constructor() {
+    console.log(this.stack, 'stack')
+  }
+  add(route: RouteLocationNormalized) {
+    console.log(route, '===312')
+    const index = this.stack.findIndex((el) => el.path === route.path)
+    if (index !== -1) {
+      this.stack.splice(index, 1)
+    }
+    this.stack.push(route)
+  }
+  jump(targetIndex: number) {
+    this.stack = this.stack.slice(0, targetIndex + 1)
+  }
+
+  getStack() {
+    return this.stack
+  }
+}
+const routeStack = new RouteStack()
+
+const router = useRouter()
+const route = useRoute()
+const currentIndex = routeStack.getStack().length - 1
+
+watch(
+  route,
+  (newRoute) => {
+    console.log(123132, '312132')
+
+    routeStack.add(newRoute)
+  },
+  { immediate: true },
+)
+
+const handleJump = (index: number) => {
+  router.push(routeStack.getStack()[index].path)
+  routeStack.jump(index)
+}
+</script>
 
 <template>
   <div class="navbar">
     <div class="leftTit">
-      <el-breadcrumb separator="/">
-        <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
-        <el-breadcrumb-item>
-          <a href="/">promotion management</a>
-        </el-breadcrumb-item>
-      </el-breadcrumb>
+      <div class="breadcrumb">
+        <span
+          v-for="(item, index) in routeStack.getStack()"
+          :key="item.path"
+          @click="handleJump(index)"
+          :class="{ active: index === currentIndex }"
+        >
+          {{ item.meta?.title || '未知' }}
+          <span v-if="index < currentIndex"> / </span>
+        </span>
+      </div>
     </div>
     <div class="rightTit">
       <!-- https://e-assets.gitee.com/gitee-community-web/_next/static/media/mjc.a8f527c7.png?x-bce-process=image -->
@@ -37,5 +88,20 @@
     // border-radius: 25px;
     // overflow: hidden;
   }
+}
+
+.breadcrumb {
+  padding: 10px;
+  background: #f5f7fa;
+  border-bottom: 1px solid #e6e9ef;
+}
+.breadcrumb span {
+  cursor: pointer;
+  margin-right: 5px;
+  color: #409eff;
+}
+.breadcrumb span.active {
+  font-weight: bold;
+  color: #303133;
 }
 </style>
