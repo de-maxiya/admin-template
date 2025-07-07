9d841cc1c832a387eedd53ef7d1c307163633857
1196683884@qq.com <1196683884@qq.com>
2025-07-07 17:42:43 +0800

    feat 新增earch图表

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -11,15 +11,6 @@
       <div>1</div>
       <div>2</div>
       <div>3</div>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
     </div>
   </div>
 </template>
@@ -34,12 +25,17 @@
 
   // auto-fill会用空格子填满剩余宽度，auto-fit则会尽量扩大单元格的宽度
   // 这个是自动填充一行  每一项宽度为100px 自动填充满，超过换行
-  // grid-template-columns: repeat(auto-fill, minmax(100px, 1fr)); //minmax(100px, 1fr)表示列宽不小于100px，不大于1fr
+  grid-template-columns: repeat(
+    auto-fill,
+    minmax(33.33%, 1fr)
+  ); //minmax(100px, 1fr)表示列宽不小于100px，不大于1fr
+  grid-template-rows: repeat(auto-fill, minmax(33.33%, 1fr));
+
   // grid-template-rows: repeat(auto-fill, minmax(100px, 1fr));
   // place-items: center; /* 同时控制项目的水平和垂直对齐（相当于 align-items + justify-items） */
 
-  grid-template-columns: 150px 1fr 2fr; // 第一列的宽度为150像素，第二列的宽度是第三列的一半。
-  grid-template-rows: 150px 1fr 2fr;
+  // grid-template-columns: 150px 1fr 2fr; // 第一列的宽度为150像素，第二列的宽度是第三列的一半。
+  // grid-template-rows: 150px 1fr 2fr;
   // 第一列和第四列的宽度为100px，第二列和第五列为20px，第三列和第六列为80px。
   // grid-template-columns: repeat(2, 100px 20px 80px); // 前面的数字是多少 就说明有几组
   gap: 10px;
@@ -48,15 +44,15 @@
   // 这个顺序由grid-auto-flow属性决定，默认值是row，即"先行后列"。也可以将它设成column，变成"先列后行"。
   // grid-auto-flow: column;
   // 设为row dense，表示"先行后列"，并且尽可能紧密填满，尽量不出现空格。
-  grid-auto-flow: row dense;
+  // grid-auto-flow: row dense;
   // 如果将设置改为column dense，表示"先列后行"，并且尽量填满空格。
   //  grid-row-gap: 20px;
   // grid-column-gap: 20px;
   // row-gap  column-gap
   div {
-    // text-align: center;
+    text-align: center;
     // line-height: 100px;
-    // background-color: #f0f0f0;
+    background-color: #f0f0f0;
     border: 1px solid #000;
   }
 }
