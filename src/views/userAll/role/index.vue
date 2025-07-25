6e65a4fbea7624c37da2c60741a8210e8aafa87d
1196683884@qq.com <1196683884@qq.com>
2025-07-25 14:43:28 +0800

    feat 新增部分接口

--- src/views/userAll/role/index.vue
+++ src/views/userAll/role/index.vue
@@ -1,36 +1,97 @@
 <template>
+  <el-button type="primary" @click="dialogVisible = true">新增</el-button>
+  <el-button type="primary" @click="getList()">搜索</el-button>
   <el-table :data="tableData" style="width: 100%">
     <el-table-column prop="id" label="ID" width="180" />
     <el-table-column prop="name" label="用户名称" width="180" />
-    <el-table-column prop="url" label="用户头像" />
+    <el-table-column prop="img" label="用户头像">
+      <template #default="scope">
+        <el-image style="width: 100px; height: 100px" :src="scope.row.img"></el-image>
+      </template>
+    </el-table-column>
   </el-table>
+
+  <el-dialog v-model="dialogVisible" title="新增" width="30%" :before-close="handleClose">
+    <el-form ref="form" :model="formall" label-width="80px" label-position="top">
+      <el-form-item label="名称">
+        <el-input v-model="formall.name" placeholder="请输入名称"></el-input>
+      </el-form-item>
+      <el-form-item label="年龄">
+        <el-input v-model="formall.age" placeholder="请输入名称"></el-input>
+      </el-form-item>
+      <el-form-item label="状态">
+        <el-input v-model="formall.status" placeholder="请输入名称"></el-input>
+      </el-form-item>
+      <el-form-item label="性别">
+        <el-input v-model="formall.gender" placeholder="请输入名称"></el-input>
+      </el-form-item>
+    </el-form>
+
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="handleAddRole"> 新增 </el-button>
+      </div>
+    </template>
+  </el-dialog>
 </template>
 <script lang="ts" setup>
-import { ref } from 'vue'
+import axios from 'axios'
+import { ref, onMounted, reactive } from 'vue'
+import { ElMessage } from 'element-plus'
 const tableData = ref([])
+const dialogVisible = ref(false)
+let formall = reactive({
+  name: '',
+  age: '',
+  status: '',
+  gender: '',
+})
 
-// const tableData = [
-//   {
-//     date: '2016-05-03',
-//     name: 'Tom',
-//     address: 'No. 189, Grove St, Los Angeles',
-//   },
-//   {
-//     date: '2016-05-02',
-//     name: 'Tom',
-//     address: 'No. 189, Grove St, Los Angeles',
-//   },
-//   {
-//     date: '2016-05-04',
-//     name: 'Tom',
-//     address: 'No. 189, Grove St, Los Angeles',
-//   },
-//   {
-//     date: '2016-05-01',
-//     name: 'Tom',
-//     address: 'No. 189, Grove St, Los Angeles',
-//   },
-// ]
+onMounted(async () => {
+  await getList()
+})
+
+const getList = async () => {
+  await axios.get('/admin/api/user').then((res) => {
+    tableData.value = res.data.data
+    console.log(res, '==321')
+  })
+}
+
+const handleClose = () => {
+  formall = {
+    name: '',
+    age: '',
+    status: '',
+    gender: '',
+  }
+  dialogVisible.value = false
+}
+// 'http://p3.music.126.net/T-pj5y9uahizQ6UhUygYdg==/109951171457557274.jpg?imageView=&thumbnail=336y336&type=webp&rotate=0&tostatic=0'
+// https://p1.music.126.net/n_GTguQS1xx9W81K-uoxww==/109951170687031355.jpg?imageView=&thumbnail=336y336&type=webp&rotate=0&tostatic=0
+// http://p3.music.126.net/e85aIwIu-ePI5xBO_C90DA==/109951171393072843.jpg?imageView=&thumbnail=336y336&type=webp&rotate=0&tostatic=0
+
+const handleAddRole = () => {
+  console.log('新增')
+  axios
+    .post('/admin/api/user', {
+      name: '666',
+      age: '111',
+      status: '1',
+      gender: '1',
+      img: 'http://p3.music.126.net/e85aIwIu-ePI5xBO_C90DA==/109951171393072843.jpg?imageView=&thumbnail=336y336&type=webp&rotate=0&tostatic=0',
+    })
+    .then((res) => {
+      if (res.data.code === 200) {
+        ElMessage({
+          message: '新增成功',
+          type: 'success',
+        })
+        getList()
+      }
+    })
+}
 </script>
 
 <style scoped lang="scss"></style>
