fce14f679aac3bbb315cb856175f96e62d6edb4c
1196683884@qq.com <1196683884@qq.com>
2025-08-04 16:52:04 +0800

    fix 新增部分筛选搜索

--- src/views/userAll/role/index.vue
+++ src/views/userAll/role/index.vue
@@ -1,6 +1,37 @@
 <template>
-  <el-button type="primary" @click="dialogVisible = true">新增</el-button>
-  <el-button type="primary" @click="getList()">搜索</el-button>
+  <el-form ref="form" :model="formSearch" label-width="80px" label-position="top">
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <el-form-item label="ID">
+          <el-input v-model="formSearch.id" placeholder=""></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="6">
+        <el-form-item label="用户名称">
+          <el-input v-model="formSearch.name" placeholder=""></el-input> </el-form-item
+      ></el-col>
+      <el-col :span="6">
+        <el-form-item label="用户年龄">
+          <el-input v-model="formSearch.age" placeholder=""></el-input> </el-form-item
+      ></el-col>
+      <el-col :span="6">
+        <el-form-item label="用户状态">
+          <el-input v-model="formSearch.status" placeholder=""></el-input> </el-form-item
+      ></el-col>
+      <el-col :span="6">
+        <el-form-item label="用户性别">
+          <el-input v-model="formSearch.gender" placeholder=""></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="6">
+        <div style="margin-top: 30px">
+          <el-button type="primary" @click="dialogVisible = true">新增</el-button>
+          <el-button type="primary" @click="getList()">搜索</el-button>
+        </div>
+      </el-col>
+    </el-row>
+  </el-form>
+
   <el-table v-loading="loading" :data="tableData" style="width: 100%">
     <el-table-column prop="id" label="ID" width="180" />
     <el-table-column prop="name" label="用户名称" width="180" />
@@ -74,6 +105,13 @@ import { ref, onMounted, reactive } from 'vue'
 import { ElMessage, ElMessageBox } from 'element-plus'
 const tableData = ref([])
 const dialogVisible = ref(false)
+const formSearch = reactive({
+  id: '',
+  age: '',
+  name: '',
+  status: '',
+  gender: '',
+})
 let formall = reactive({
   name: '',
   age: '',
@@ -113,17 +151,33 @@ const genderOptions = [
 onMounted(async () => {
   await getList()
 })
+const pageAll = ref({
+  page: 1,
+  pageSize: 10,
+})
 
+const count = ref(0)
+// 获取数据
 const getList = async () => {
   loading.value = true
-  await axios.get('/admin/api/user').then((res) => {
-    tableData.value = res.data.data
-    loading.value = false
-  })
+  await axios
+    .get('/admin/api/user', {
+      params: {
+        ...formSearch,
+        page: pageAll.value.page,
+        pageSize: pageAll.value.pageSize,
+      },
+    })
+    .then((res) => {
+      tableData.value = res.data.data.list
+      count.value = res.data.data.count
+      loading.value = false
+    })
 }
 
 const loading = ref(false)
 
+// 关闭
 const handleClose = () => {
   Object.assign(formall, {
     name: '',
@@ -135,10 +189,8 @@ const handleClose = () => {
   dialogVisible.value = false
   isEdit.value = false
 }
-// 'http://p3.music.126.net/T-pj5y9uahizQ6UhUygYdg==/109951171457557274.jpg?imageView=&thumbnail=336y336&type=webp&rotate=0&tostatic=0'
-// https://p1.music.126.net/n_GTguQS1xx9W81K-uoxww==/109951170687031355.jpg?imageView=&thumbnail=336y336&type=webp&rotate=0&tostatic=0
-// http://p3.music.126.net/e85aIwIu-ePI5xBO_C90DA==/109951171393072843.jpg?imageView=&thumbnail=336y336&type=webp&rotate=0&tostatic=0
 
+// 新增
 const handleAddRole = () => {
   axios
     .post('/admin/api/user', {
@@ -162,6 +214,7 @@ const handleAddRole = () => {
     })
 }
 
+// 编辑
 interface RowType {
   id: number
   name: string
@@ -177,6 +230,7 @@ const handleEdit = (row: RowType) => {
   formall = row
 }
 
+//  删除
 const handleDelete = (row: RowType) => {
   ElMessageBox.confirm('确认删除吗？', 'Warning', {
     confirmButtonText: '确定',
