9d369e7d13a4b4738192266665c173d2094ab9f2
1196683884@qq.com <1196683884@qq.com>
2025-08-13 17:00:08 +0800

    fix 优化完成左侧siderbar

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -81,10 +81,10 @@ const handleCloseOpen = () => {
       <template v-for="item in menuItems" :key="item.path || item.index">
         <!-- 渲染一级菜单项 -->
         <el-menu-item v-if="item.type === 'item'" :index="item.path">
+          <el-icon>
+            <component :is="item.meta.icon" />
+          </el-icon>
           <template #title>
-            <el-icon>
-              <component :is="item.meta.icon" />
-            </el-icon>
             <span>{{ item.meta.title }}</span>
           </template>
         </el-menu-item>
